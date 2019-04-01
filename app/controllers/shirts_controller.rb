9ddc0aadeae74a0f89ea80c4f16c50446c0d1ba3
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-01 11:21:42 +0200

    added index and show to shirts  controller, workign

---
+++ app/controllers/shirts_controller.rb
@@ -0,0 +1,14 @@
+class ShirtsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @shirts = Shirt.all
+
+  end
+
+  def show
+    @shirt = Shirt.find(params[:id])
+
+  end
+
+end
