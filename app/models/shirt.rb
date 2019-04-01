28058cb5083c327fa7ec4bb5b39d3efe9d3a36e7
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-01 11:31:33 +0200

    -

--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -2,5 +2,6 @@ class Shirt < ApplicationRecord
   belongs_to :category
   validates :sku, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
+  monetize :price_cents
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@ casual = Category.create!(name: 'casual')
 
 
 puts 'Creating Shirts..'
-Shirt.create!(sku: 'original-shirt-bear',  name: 'Shirt bear', category: geek, photo_url:'https://images.unsplash.com/photo-1544284490-a105bb94c991?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=401&q=80')
-Shirt.create!(sku: 'jean-mimi', name: 'Jean-Michel - Le Wagon', category: casual, photo_url: 'https://images.unsplash.com/photo-1462392246754-28dfa2df8e6b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
-Shirt.create!(sku: 'Silk-mini', name: 'Silk-mini - Silk', category: casual, photo_url: 'https://images.unsplash.com/photo-1494281258937-45f28753affd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
+
+Shirt.create!(price: 55 , sku: 'original-shirt-bear',  name: 'Shirt bear', category: geek, photo_url:'https://images.unsplash.com/photo-1544284490-a105bb94c991?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=401&q=80')
+Shirt.create!(price:30 ,sku: 'jean-mimi', name: 'Jean-Michel - Le Wagon', category: casual, photo_url: 'https://images.unsplash.com/photo-1462392246754-28dfa2df8e6b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
+Shirt.create!(price:35 ,sku: 'Silk-mini', name: 'Silk-mini - Silk', category: casual, photo_url: 'https://images.unsplash.com/photo-1494281258937-45f28753affd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
 
 puts 'Finished'
