c7eb81472185a7d820ab694e542d981f113d7e5f
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-02 15:03:45 +0200

    not workign add review, server error no clue what the fuck it that

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,42 @@
 class ReviewsController < ApplicationController
 
-  def create
+     before_action :set_shirt, only: [ :show]
+
 
     def create
-    @shirt = Shirt.find(params[:shirt_id])
+    @shirt = shirt.find(params[:shirt_id])
     @review = Review.new(review_params)
     @review.shirt = @shirt
-    if @review.save
-      redirect_to shirt_path(@shirt)
-    else
-      render 'shirt/show'
+    if @review.save!
+      respond_to do |format|
+    format.html  {redirect_to shirt_path(@shirt)}
+    format.js # will render 'app/views/reviews/create.js.erb'
+    end
+  else
+    respond_to do  |format|
+       format.html {render 'shirts/show'}
+
+       format.js # idem
+     end
     end
   end
 
+
+
+
+
   private
+
+
 
   def review_params
     params.require(:review).permit(:content)
   end
+
+
+  def set_shirt
+    @shirt = Shirt.find(params[:id])
+    authorize @shirt
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :shirts, only: [:index, :show] do
-    resources :reviews, only: :create
+  resources :reviews, only: :create
   end
   resources :orders, only: [:show, :create] do
   resources :payments, only: [:new, :create]
